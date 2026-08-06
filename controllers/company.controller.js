7ed425d34c301ea5f597ca6cb2cde645110f467e
kaioumdev <afrankaiyum34@gmail.com>
2026-08-07 05:29:51 +0600

    make getCompanyid, updateCompany function for company

--- controllers/company.controller.js
+++ controllers/company.controller.js
@@ -49,3 +49,40 @@ export const getAllCompanies = async (req, res) => {
   }
 };
 
+//get company by id
+export const getCompanyById = async (req, res) => {
+  try {
+    const companyId = req.params.id;
+    const company = await Company.findById(companyId);
+    if (!company) {
+      return res.status(404).json({ message: "Company not found" });
+    }
+    return res.status(200).json({ company, success: true });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+//update company details
+export const updateCompany = async (req, res) => {
+  try {
+    const { name, description, website, location } = req.body;
+    const file = req.file;
+    //cloudinary
+    const fileUri = getDataUri(file);
+    const cloudResponse = await cloudinary.uploader.upload(fileUri.content);
+    const logo = cloudResponse.secure_url;
+
+    const updateData = { name, description, website, location, logo };
+
+    const company = await Company.findByIdAndUpdate(req.params.id, updateData, {
+      new: true,
+    });
+    if (!company) {
+      return res.status(404).json({ message: "Company not found" });
+    }
+    return res.status(200).json({ message: "Company updated" });
+  } catch (error) {
+    console.error(error);
+  }
+};
